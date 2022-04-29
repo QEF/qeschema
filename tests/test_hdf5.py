f93d356cf3c2553ed449dbf1aa7c874478cc6fc1
Davide Brunato <brunato@sissa.it>
2022-04-29 11:40:31 +0200

    Skip tests when an extra feature is not available

--- tests/test_hdf5.py
+++ tests/test_hdf5.py
@@ -11,26 +11,31 @@ import unittest
 import numpy as np
 from pathlib import Path
 
-from qeschema.hdf5 import read_charge_file, get_wavefunctions, \
-    get_wf_attributes, get_wfc_miller_indices
+try:
+    import yaml
+except ImportError:
+    yaml = None
+else:
+    from qeschema.hdf5 import read_charge_file, get_wavefunctions, \
+        get_wf_attributes, get_wfc_miller_indices
 
 
-# TODO: Fetch appropriate HDF5 files for testing
+    # TODO: Fetch appropriate HDF5 files for testing
 
-@unittest.SkipTest
-class TestHdf5Utils(unittest.TestCase):
+    @unittest.SkipTest
+    class TestHdf5Utils(unittest.TestCase):
 
-    def test_read_charge_file(self):
-        hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
+        def test_read_charge_file(self):
+            hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
 
-    def test_get_wf_attributes(self):
-        hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
+        def test_get_wf_attributes(self):
+            hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
 
-    def test_get_wavefunctions(self):
-        hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
+        def test_get_wavefunctions(self):
+            hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
 
-    def test_get_wfc_miller_indices(self):
-        hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
+        def test_get_wfc_miller_indices(self):
+            hdf5_file = Path(__file__).parent.joinpath('resources/hdf5/???')
 
 
 if __name__ == '__main__':
